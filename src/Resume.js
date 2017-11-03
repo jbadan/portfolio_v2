f2cc43f2051ce66d11bede80be7859b46aef3fa1
jbadan <jenna.badanowski@gmail.com>
2017-11-02 17:30:40 -0700

    git pages deployment config

--- src/Resume.js
+++ src/Resume.js
@@ -8,8 +8,8 @@ class Resume extends Component {
       <div className="row fixedHeight text-center">
         <Sidebar />
         <div className="col-md-8 App">
-          <a target="_blank" href="https://drive.google.com/file/d/1ecIdovljemb5OAhK5gYYwN8uAGqaMn-d/view?usp=sharing">Click here for a downloadable pdf version</a> <br/>
-          <a target="_blank" href="https://drive.google.com/file/d/1fJtJkTjtlYD0rX5LX-KNfu7TnVlXEwuS/view?usp=sharing">  Plain text version</a>
+          <a target="_blank" href="https://drive.google.com/file/d/1ecIdovljemb5OAhK5gYYwN8uAGqaMn-d/view?usp=sharing">pdf version</a> <br/>
+          <a target="_blank" href="https://drive.google.com/file/d/1fJtJkTjtlYD0rX5LX-KNfu7TnVlXEwuS/view?usp=sharing">  .txt version</a>
           <iframe src="https://docs.google.com/document/d/e/2PACX-1vTvBYVT6cyErDUOHoRIutz-lWsCIEGd2_uTlQkxBulMwDVTjfKVSP0DG7uqY0M9en57G4JirEoLv_Ik/pub?embedded=true"></iframe>
         </div>
       </div>
