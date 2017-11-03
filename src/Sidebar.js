f56988ec1842d229772bf4238dec74cf04c96968
jbadan <jenna.badanowski@gmail.com>
2017-11-03 14:48:13 -0700

    added transitions

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import './Home.css';
 import jenna from './jenna.jpg';
+import Sticky from 'react-stickynode';
 import{
   BrowserRouter as Router,
   Link
@@ -10,46 +11,52 @@ import{
 class Sidebar extends Component {
   render() {
     return (
-        <div className="col-md-4 my-auto mx-auto">
-          <div className="row minHeightRow">
-            <div className="col-md-12">
-              <img className="logoImage" alt="jenna" src={jenna} />
-            </div>
-          </div>
-          <div className="row text-center">
-            <div className="col-md-12">
-              <h1 className="white"> Jenna Badanowski</h1>
-            </div>
-          </div>
-          <div className="row text-center">
-            <div className="col-md-12">
-              <h2 className="white">Web Developer</h2>
-            </div>
-          </div>
-          <div className="row text-center">
-            <div className="col-md-12">
-              <Link to="/about">
-                  <button type="button">About</button>
-              </Link>
-              <Link to="/work">
-                  <button type="button">Projects</button>
-              </Link>
-              <Link to="/resume">
-                  <button type="button">Resume</button>
-              </Link>
-              <Link to="/contact">
-                  <button type="button">Contact</button>
-              </Link>
+        <div className="col-md-4 mx-auto">
+          <Sticky enabled={true} top={150}>
+            <div>
+              <div className="row minHeightRow">
+                <div className="col-md-12">
+                  <img className="logoImage" alt="jenna" src={jenna} />
+                </div>
+              </div>
+              <div className="row text-center">
+                <div className="col-md-12">
+                  <h1 className="white"> Jenna Badanowski</h1>
+                </div>
+              </div>
+              <div className="row text-center">
+                <div className="col-md-12">
+                  <h2 className="white">Web Developer</h2>
+                </div>
+              </div>
+              <div className="row text-center">
+                <div className="col-md-12">
+                  <Link to="/about">
+                      <button type="button">About</button>
+                  </Link>
+                  <Link to="/work">
+                      <button type="button">Projects</button>
+                  </Link>
+                  <Link to="/resume">
+                      <button type="button">Resume</button>
+                  </Link>
+                  <Link to="/contact">
+                      <button type="button">Contact</button>
+                  </Link>
 
+                </div>
+              </div>
+              <div className="row text-center">
+                <div className="col-md-12">
+                  <a target="_blank" href="https://www.linkedin.com/in/jenna-badanowski-6b672261/"><i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
+                  <a target="_blank" href="https://drive.google.com/file/d/1ecIdovljemb5OAhK5gYYwN8uAGqaMn-d/view?usp=sharing"><i className="fa fa-file-o fa-2x" aria-hidden="true"></i></a>
+                  <a target="_blank" href="https://github.com/jbadan"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="row text-center">
-            <div className="col-md-12">
-              <a target="_blank" href="https://www.linkedin.com/in/jenna-badanowski-6b672261/"><i className="fa fa-linkedin-square fa-2x" aria-hidden="true"></i></a>
-              <a target="_blank" href="https://drive.google.com/file/d/1ecIdovljemb5OAhK5gYYwN8uAGqaMn-d/view?usp=sharing"><i className="fa fa-file-o fa-2x" aria-hidden="true"></i></a>
-              <a target="_blank" href="https://github.com/jbadan"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a>
-            </div>
-          </div>
+
+          </Sticky>
+
         </div>
 
     );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import { render } from "react-dom";
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Home from "./Home";
+import About from "./About";
+import Work from  "./Work";
+import Contact from "./Contact";
+import NotFound from "./NotFound";
+import Resume from "./Resume";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import anime from "animejs";
+import "./fade.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import{
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+}from 'react-router-dom';
+
+const PageFade = props => (
+  <CSSTransition
+    {...props}
+    classNames="fadeTranslate"
+    timeout={1000}
+    mountOnEnter={true}
+    unmountOnExit={true}
+  />
+);
+
+const App = props => {
+
+  const locationKey = props.location.pathname;
+
+    return (
+      <TransitionGroup>
+       <PageFade key={locationKey}>
+         <section className="fix-container">
+          <Switch location ={props.location}>
+           <Route exact path="/" component={Home} />
+           <Route path="/about" component={About} />
+           <Route path="/work" component={Work} />
+           <Route path="/contact" component={Contact} />
+           <Route path="/resume" component={Resume} />
+           <Route component={NotFound} />
+         </Switch>
+        </section>
+       </PageFade>
+     </TransitionGroup>
+    );
+}
+const BasicExample = () => (
+  <Router>
+    <Route path="/" component={App} />
+  </Router>
+);
+
+
+render(<BasicExample />, document.getElementById("root"));
