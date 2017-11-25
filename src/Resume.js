591061ffbe407c7a5c14527b4f3aaea67cd806a8
jbadan <jenna.badanowski@gmail.com>
2017-11-25 15:28:16 -0800

    added dropdown menu on resume section

--- src/Resume.js
+++ src/Resume.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Navbar from './Navbar';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import resume from './resume.png';
 
 class Resume extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+  toggle() {
+   this.setState({
+     dropdownOpen: !this.state.dropdownOpen
+   });
+ }
   render() {
     return (
       <div className="row fixedHeight text-center">
@@ -16,14 +29,19 @@ class Resume extends Component {
           </div>
         <div className="col-md-8 App">
           <div className="row">
-            <div className="col-md-2"/>
+            <div className="col-md-8"/>
             <div className="col-md-4">
-              <a rel="noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1ecIdovljemb5OAhK5gYYwN8uAGqaMn-d/view?usp=sharing">Download pdf <i className="fa fa-file-pdf-o" aria-hidden="true"></i></a>
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                <DropdownToggle caret>
+                  Download
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem><a rel="noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1ecIdovljemb5OAhK5gYYwN8uAGqaMn-d/view?usp=sharing">Download pdf <i className="fa fa-file-pdf-o" aria-hidden="true"></i></a></DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>< a rel="noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1fJtJkTjtlYD0rX5LX-KNfu7TnVlXEwuS/view?usp=sharing">Download plain text <i className="fa fa-arrow-down" aria-hidden="true"></i></a></DropdownItem>
+                </DropdownMenu>
+              </Dropdown>
             </div>
-            <div className="col-md-4">
-            < a rel="noopener noreferrer" target="_blank" href="https://drive.google.com/file/d/1fJtJkTjtlYD0rX5LX-KNfu7TnVlXEwuS/view?usp=sharing">Download plain text <i className="fa fa-arrow-down" aria-hidden="true"></i></a>
-            </div>
-            <div className="col-md-2"/>
           </div>
           <div className="row">
             <div className="col-md-1"/>
